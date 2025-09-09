cc73795a1ff88ba5157ac2fd789ab5607e082ef0
fish-es <3168347190@qq.com>
2025-09-09 15:10:45 +0800

    打包后文件路径更改问题

--- src/main/java/h537/first/controller/first_test.java
+++ src/main/java/h537/first/controller/first_test.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+
+//needed remove
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+
+import java.io.InputStream;
 import java.util.Base64;
 
 
@@ -21,11 +25,22 @@ public class first_test {
     @GetMapping("/get_img")
     public String getImg() throws IOException {
         log.info("get img");
-        String imagePath = "src/main/resources/static/1.jpg";
-        Path path = Path.of(imagePath);
+
+//        String imagePath = "src/main/resources/static/1.jpg";
+//        Path path = Path.of(imagePath);
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("static/1.jpg");
+        if (inputStream == null) {
+            throw new IOException("File not found: static/1.jpg");
+        }
 
         // Read the file content into a byte array1
-        byte[] fileContent = Files.readAllBytes(path);
+
+//        byte[] fileContent = Files.readAllBytes(path);
+
+        byte[] fileContent = new byte[inputStream.available()];
+        inputStream.read(fileContent);
+        inputStream.close();
 
         // Encode the byte array to a Base64 string11111
         String base64EncodedString = Base64.getEncoder().encodeToString(fileContent);
