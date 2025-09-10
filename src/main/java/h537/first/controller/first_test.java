93c21746696b97867604a099b5c7284112591369
fish-es <3168347190@qq.com>
2025-09-10 16:26:58 +0800

    这个版本依然在报错

--- src/main/java/h537/first/controller/first_test.java
+++ src/main/java/h537/first/controller/first_test.java
@@ -16,33 +16,9 @@ import java.util.Base64;
 
 public class first_test {
 
-
-//    @GetMapping("/get_img")
-//    public String getImg() throws IOException {
-//        log.info("get img");
-//
-//        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("static/1.jpg");
-//        if (inputStream == null) {
-//            throw new IOException("File not found: static/4568.jpg");
-//        }
-//        // Read the file content into a byte array1
-//
-//        byte[] fileContent = inputStream.readAllBytes();
-//        inputStream.read(fileContent);
-//        inputStream.close();
-//
-//        // Encode the byte array to a Base64 string11111
-//        String base64EncodedString = Base64.getEncoder().encodeToString(fileContent);
-//
-//        // Print Base64 string to the console
-//        System.out.println(base64EncodedString);
-//
-//
-//        return base64EncodedString;
-//    }
     @GetMapping("/get_img")
     public String getImg() throws IOException {
-        log.info("666");
+        log.info("1116");
         return "6666666666666666";
 
     }
