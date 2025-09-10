bd86b73109146e0b6e3cf0b73b33fa8378016780
fish-es <3168347190@qq.com>
2025-09-10 15:59:22 +0800

    解决了跨域问题

---
+++ src/main/java/h537/first/peizhi/WebConfig.java
@@ -0,0 +1,19 @@
+package h537.first.peizhi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 允许的前端域名和端口
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
